a8f004188e1d1509f4ea77627c699a3860cee458
Markgrave <bogdanrudenko404@gmail.com>
2026-02-20 15:43:56 +0000

    feat(store): add store persistence, delete unused setQuestions

--- src/lib/store.js
+++ src/lib/store.js
@@ -1,29 +1,37 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
-export const useQuizStore = create((set, get) => ({
-  questions: [],
-  status: "idle",
-  settings: {
-    category: null,
-    amount: 0,
-    difficulty: "",
-  },
-  score: 0,
-  error: null,
-  timeLeft: 30,
-  currentIndex: 0,
-  answers: [],
-
-  setQuestions: (questions) => set({ questions }),
-  setSettings: (newSettings) =>
-    set((state) => ({ settings: { ...state.settings, ...newSettings } })),
-  startQuiz: (questions) =>
-    set({
-      status: "active",
-      questions: questions,
-      currentIndex: 0,
+export const useQuizStore = create(
+  persist(
+    (set, get) => ({
+      questions: [],
+      status: "idle",
+      settings: {
+        categoryIndex: null,
+        categoryLabel: "",
+        amount: 0,
+        difficulty: "",
+      },
       score: 0,
+      error: null,
       timeLeft: 30,
+      currentIndex: 0,
       answers: [],
+
+      setSettings: (newSettings) =>
+        set((state) => ({ settings: { ...state.settings, ...newSettings } })),
+      startQuiz: (questions) =>
+        set({
+          status: "active",
+          questions: questions,
+          currentIndex: 0,
+          score: 0,
+          timeLeft: 30,
+          answers: [],
+        }),
     }),
-}));
+    {
+      name: "quiz-storage",
+    },
+  ),
+);
