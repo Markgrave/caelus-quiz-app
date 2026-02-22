aa3e8b03a9cb1d330fdb7580aab5f7395169c0cc
Markgrave <bogdanrudenko404@gmail.com>
2026-02-22 22:20:21 +0000

    feat(Quiz page): implement quiz page, add question card component, update routing and packages, button component

--- src/components/ui/Button/index.jsx
+++ src/components/ui/Button/index.jsx
@@ -1,13 +1,16 @@
 import styles from "./Button.module.scss";
 import classnames from "classnames";
 
-const Button = ({ disabled = false, children, onClick }) => {
+const Button = ({ disabled = false, children, onClick, type }) => {
   const buttonClass = classnames(styles.button, {
     [styles.disabled]: disabled,
+    [styles.answer]: type === "answer",
+    [styles.right]: type === "answerRight",
+    [styles.wrong]: type === "answerWrong",
   });
 
   return (
-    <button className={buttonClass} onClick={onClick}>
+    <button className={buttonClass} onClick={onClick} disabled={disabled}>
       <p className={styles.content}>{children}</p>
     </button>
   );
