bf0dccf5b9f3a23e88808191e88e5a0d89cd8355
Markgrave <bogdanrudenko404@gmail.com>
2026-03-01 20:31:30 +0000

    feat(header): add routing to home page on click

--- src/components/ui/Header/index.jsx
+++ src/components/ui/Header/index.jsx
@@ -1,10 +1,13 @@
 import styles from "./Header.module.scss";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <header className={styles.headerWrapper}>
       <div className={styles.header}>
-        <h1 className={styles.title}>Caelus.</h1>
+        <Link to="/" className={styles.link}>
+          <h1 className={styles.title}>Caelus.</h1>
+        </Link>
       </div>
     </header>
   );
